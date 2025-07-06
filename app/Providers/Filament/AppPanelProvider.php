e88ad29cdf12726924c0e37f7c873297d7d9f881
M. Aditia Firmansyah <aditia.pikarin@gmail.com>
2025-07-06 14:18:59 +0700

    Fix typo in color definition for Filament panel

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -31,7 +31,7 @@ class AppPanelProvider extends PanelProvider
             ->login()
             ->colors([
                 'primary' => Color::Indigo,
-                'gway' => Color::Slate,
+                'gray' => Color::Slate,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
